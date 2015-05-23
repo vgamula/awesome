16df2c42d444c0d80135373244698b9b0650864f
Volodymyr Gamula <v.gamula@gmail.com>
2015-05-23 15:27:49 +0300

    Added map for event edit/create api

--- src/views/event/_form.php
+++ src/views/event/_form.php
@@ -32,6 +32,12 @@ use yii\widgets\ActiveForm;
     ]) ?>
 
 
+    <?= $form->field($model, 'lat')->textInput(['maxlength' => true, 'id'=>'lat']) ?>
+    <?= $form->field($model, 'lng')->textInput(['maxlength' => true, 'id'=>'lng']) ?>
+
+    <div id="map" style="width: 500px; height: 380px;">
+    </div>
+
     <?= $form->field($model, 'visible')->dropDownList(Event::getVisibilities()) ?>
 
     <?= $form->field($model, 'status')->dropDownList(Event::getStatuses()) ?>
@@ -41,5 +47,48 @@ use yii\widgets\ActiveForm;
     </div>
 
     <?php ActiveForm::end(); ?>
+
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+    <script type="text/javascript">
+        function setPosition(marker, pos, map) {
+            marker.setMap(null);
+            marker.position = pos;
+            marker.setMap(map);
+            document.getElementById("lat").value = pos.lat();
+            document.getElementById("lng").value = pos.lng();
+        }
+        function initialize(){
+            var marker = null;
+            var centerLatLng = new google.maps.LatLng(51.508742, -0.120850);
+            var mapProp = {
+                center: centerLatLng,
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = window.map = new google.maps.Map(document.getElementById("map"), mapProp);
+            marker = new google.maps.Marker({
+                position: centerLatLng,
+                map: map,
+                draggable:true,
+            });
+            google.maps.event.addListener(marker, 'mouseup', function(event) {
+                setPosition(marker, event.latLng, map);
+            });
+            google.maps.event.addListener(map, "click", function(event)
+            {
+                setPosition(marker, event.latLng, map);
+            });
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(pos) {
+                    coords = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                    setPosition(marker, coords, map);
+                    map.setCenter(coords);
+                });
+            }
+        };
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+
+
 
 </div>
