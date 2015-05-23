1d271ff8866540b747e7670fac9b86167967f4b6
Ruslan Porchinskiy <ruslan@MacBook-Pro-Volodymyr.local>
2015-05-23 19:01:12 +0300

    mail

--- src/commands/ScheduleController.php
+++ src/commands/ScheduleController.php
@@ -1,8 +1,11 @@
 <?php
 namespace app\commands;
- 
+
 use yii\console\Controller;
- 
+use app\models\Event;
+use app\models\EventSearch;
+use app\models\User;
+
 /**
  *
  */
@@ -12,5 +15,21 @@ class ScheduleController extends Controller {
 
     public function actionIndex()
     {
+
+      $date = new \DateTime();
+      // $date->add(new \DateInterval('P10D'));
+      //var_dump($date);
+      $events = Event::find()
+       ->where(['>=', 'start', $date->format("Y-m-d H:i:s")])
+       ->joinWith(['users'])
+       ->all();
+
+
+
+      foreach ($events as $event) {
+        // echo $event->email;
+        var_dump($event);
+      }
+      // var_dump($events);
     }
 }
